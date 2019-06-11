9ea357bdefb034e717915956f13edce1c89eab86
chensi <627565157@qq.com>
2019-06-11 10:12:59 +0800

    project init and first arithmetic

---
+++ src/com/chensi/arithmetic/ReverseWordInSentence.java
@@ -0,0 +1,14 @@
+package com.chensi.arithmetic;
+
+/**
+ * Reverse each word in a sentence
+ * @Author: chensi
+ * @Date: 2019/6/11 10:08
+ * @Version 1.0
+ */
+public class ReverseWordInSentence {
+
+    public static void main(String[] args) {
+
+    }
+}
