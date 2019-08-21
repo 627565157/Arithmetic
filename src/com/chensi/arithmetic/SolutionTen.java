1a02857d132517d7bb9cc79f395b4cdfeec94351
chensi <627565157@qq.com>
2019-08-21 13:11:37 +0800

    两数之和 两数相加 无重复字符的最长子串 寻找两个有序数组的中位数 Z 字形变换

--- src/com/chensi/arithmetic/SolutionTen.java
+++ src/com/chensi/arithmetic/SolutionTen.java
@@ -1,5 +1,7 @@
 package com.chensi.arithmetic;
 
+import java.util.*;
+
 /**
  * @author chensi
  * 2019-08-19 09:30
@@ -7,12 +9,131 @@ package com.chensi.arithmetic;
 public class SolutionTen {
 
     public static void main(String[] args) {
-        int[] num1 = new int[]{1, 2};
-        int[] num2 = new int[]{3, 4};
+        String str = "LEETCODEISHIRING";
         SolutionTen solutionTen = new SolutionTen();
-        System.out.println(solutionTen.findMedianSortedArrays(num1, num2));
+        System.out.println(solutionTen.convert(str, 4));
     }
 
+    /**
+     * 两数之和
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int subResult = target - num;
+            if (map.containsKey(subResult)) {
+                return new int[]{map.get(subResult), i};
+            }
+            map.put(num, i);
+        }
+        throw new IllegalArgumentException("no result");
+    }
+
+
+    //     * Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 两数相加
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     * <p>
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     * <p>
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode(0);
+        int sum = 0;
+        while (true) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            node.val = sum % 10;
+            sum = sum / 10;
+            if (l1 != null || l2 != null || sum != 0) {
+                node.next = new ListNode(0);
+                node = node.next;
+            } else {
+                break;
+            }
+        }
+        return node;
+    }
+
+
+    /**
+     * 无重复字符的最长子串
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * <p>
+     * 示例 2:
+     * <p>
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * <p>
+     * 示例 3:
+     * <p>
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        int[] index = new int[128];
+        for (int i = 0, j = 0; j < n; j++) {
+            i = Math.max(i, index[s.charAt(j)]);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
 
     /**
      * 解法1
@@ -60,6 +181,7 @@ public class SolutionTen {
      * 解法2
      * 寻找两个有序数组的中位数
      * 只遍历， 不创建新数组
+     *
      * @param nums1
      * @param nums2
      * @return
@@ -89,8 +211,9 @@ public class SolutionTen {
 
     /**
      * 解法3 寻找两个有序数组的中位数
-     *
+     * <p>
      * 利用查找第k个小的元素，每次舍弃小于第k/2个元素的元素
+     *
      * @param nums1
      * @param nums2
      * @return
@@ -107,6 +230,7 @@ public class SolutionTen {
     /**
      * 空间复杂度：虽然我们用到了递归，但是可以看到这个递归属于尾递归，所以编译器不需要不停地堆栈，所以空间复杂度为 O(1)
      * 找到第K个小的值
+     *
      * @param nums1
      * @param start1
      * @param end1
@@ -139,6 +263,7 @@ public class SolutionTen {
     /**
      * 解法4 寻找两个有序数组的中位数
      * i + j = m - i  + n - j  , 也就是 j = ( m + n ) / 2 - i
+     *
      * @param nums1
      * @param nums2
      * @return
@@ -146,35 +271,35 @@ public class SolutionTen {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        if(m > n) {
+        if (m > n) {
             return findMedianSortedArrays(nums2, nums1);
         }
         int iMin = 0;
         int iMax = m;
-        while(iMin <= iMax) {
-            int i = (iMin + iMax) /2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
             int j = (m + n + 1) / 2 - i;
-            if(j != 0 && i != m && nums2[j - 1] > nums1[i]) {
+            if (j != 0 && i != m && nums2[j - 1] > nums1[i]) {
                 iMin = i + 1;
-            } else if(i != 0 && j != n && nums1[i - 1] > nums2[j]) {
+            } else if (i != 0 && j != n && nums1[i - 1] > nums2[j]) {
                 iMax = i - 1;
             } else {
                 int maxLeft;
-                if(i == 0) {
+                if (i == 0) {
                     maxLeft = nums2[j - 1];
-                } else if(j == 0) {
+                } else if (j == 0) {
                     maxLeft = nums1[i - 1];
                 } else {
                     maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
-                if((m + n) % 2 == 1) {
+                if ((m + n) % 2 == 1) {
                     return maxLeft;
                 }
 
                 int minRight;
-                if(i == m) {
+                if (i == m) {
                     minRight = nums2[j];
-                } else if(j == n) {
+                } else if (j == n) {
                     minRight = nums1[i];
                 } else {
                     minRight = Math.min(nums1[i], nums2[j]);
@@ -185,5 +310,113 @@ public class SolutionTen {
         return 0.0;
     }
 
+
+    /**
+     * 最长回文子串
+     *
+     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     * <p>
+     * 示例 2：
+     * <p>
+     * 输入: "cbbd"
+     * 输出: "bb"
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        String longestPllindStr = s.substring(0, 1);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String test = s.substring(i, j);
+                if (isPlindStr(test) && test.length() >= longestPllindStr.length()) {
+                    longestPllindStr = test;
+                }
+            }
+        }
+        return longestPllindStr;
+    }
+
+    private boolean isPlindStr(String str) {
+        if (str == null || str.length() < 2) {
+            return false;
+        }
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Z 字形变换
+     *
+     * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+     *
+     * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+     *
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     *
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+     *
+     * 请你实现这个将字符串进行指定行数变换的函数：
+     *
+     * string convert(string s, int numRows);
+     *
+     * 示例 1:
+     *
+     * 输入: s = "LEETCODEISHIRING", numRows = 3
+     * 输出: "LCIRETOESIIGEDHN"
+     *
+     * 示例 2:
+     *
+     * 输入: s = "LEETCODEISHIRING", numRows = 4
+     * 输出: "LDREOEIIECIHNTSG"
+     * 解释:
+     *
+     * L     D     R
+     * E   O E   I I
+     * E C   I H   N
+     * T     S     G
+     *
+     * @param s
+     * @param numRows
+     * @return
+     */
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(s.length(), numRows); i++) {
+            rows.add(new StringBuilder());
+        }
+        boolean isDown = false;
+        int curRow = 0;
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) {
+                isDown = !isDown;
+            }
+            curRow += isDown ? 1 : -1;
+        }
+        StringBuilder root = new StringBuilder();
+        for (StringBuilder row : rows) {
+            root.append(row);
+        }
+        return root.toString();
+    }
 
 }
