7d459273964968d62c293b4f3a3b6e58c426cd2a
chensi <627565157@qq.com>
2019-08-21 16:25:16 +0800

    回文数

--- src/com/chensi/arithmetic/SolutionTen.java
+++ src/com/chensi/arithmetic/SolutionTen.java
@@ -11,7 +11,8 @@ public class SolutionTen {
     public static void main(String[] args) {
         SolutionTen solutionTen = new SolutionTen();
         String str = "+2+2";
-        System.out.println(solutionTen.myAtoi(str));
+        int x = 1221;
+        System.out.println(solutionTen.isPalindrome(x));
     }
 
     /**
@@ -559,6 +560,48 @@ public class SolutionTen {
             }
         }
         return ato;
+    }
+
+    /**
+     * 回文数
+     *
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     *
+     * 示例 1:
+     *
+     * 输入: 121
+     * 输出: true
+     *
+     * 示例 2:
+     *
+     * 输入: -121
+     * 输出: false
+     * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     *
+     * 示例 3:
+     *
+     * 输入: 10
+     * 输出: false
+     * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+     *
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        if(x <= 9) {
+            return true;
+        }
+        String str = String.valueOf(x);
+        int len = str.length();
+        for(int i = 0 ; i < len / 2; i++) {
+            if(str.charAt(i) != str.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
